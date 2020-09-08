4418a2441ed530c4d73921b8dc947e2acd9be667
mahendra319 <mahendra319@gmail.com>
2020-09-09 00:21:20 +0530

    modified

--- src/main/java/com/qa/hubspot/pages/CompaniesPage.java
+++ src/main/java/com/qa/hubspot/pages/CompaniesPage.java
@@ -6,7 +6,7 @@ public class CompaniesPage {
 	public CompaniesPage() {
 		System.out.println("Companies Page to be design ");
 		System.out.println("page of companies to be design ");
-		System.out.println("test for auto build run from jenkins when the momemnt we chekin code into git hub");
+		//System.out.println("test for auto build run from jenkins when the momemnt we chekin code into git hub");
 	}
 
 }
